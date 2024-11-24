7bbbf8916f21849d11527f6d137ff27a8dda2ca7
antony2317 <anton43234@gmail.com>
2024-11-24 12:56:08 +0300

    задача номер 2

---
+++ lesson_6/task_2.py
@@ -0,0 +1,6 @@
+print("""     Задача № 2
+Программа переводит число из десятичной системы в двоичную""")
+def dec_bin(number):
+    return bin(number)[2:]
+number = int(input('Введите число: '))
+print(dec_bin(number))
