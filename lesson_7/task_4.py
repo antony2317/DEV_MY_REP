102856ffb4c5930053523c7b0bcd5b09e4cba5fb
antony2317 <anton43234@gmail.com>
2024-11-30 20:33:26 +0300

    задача номер 4

---
+++ lesson_7/task_4.py
@@ -0,0 +1,17 @@
+import time
+
+
+def timer(f):
+    def tmp(*args, **kwargs):
+        t = time.time()
+        res = f(*args, **kwargs)
+        print("Время выполнения функции: %f" % (time.time() - t))
+        return res
+
+    return tmp
+
+
+@timer
+def func(x, y):
+    return x + y
+print(func(52,56))
