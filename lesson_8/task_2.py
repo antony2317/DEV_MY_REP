49370b0654b1735451daa6e5766b8bf0977709fd
antony2317 <anton43234@gmail.com>
2024-12-01 21:25:48 +0300

    задача номер 2 калькулятор

---
+++ lesson_8/task_2.py
@@ -0,0 +1,25 @@
+print('Для завершения программы введите 0')
+
+while True:
+    s = input('Выберете математическую операцию (+, -, *, /): ')
+    if s == '0':
+        break
+    if s in ('+', '-', '*', '/'):
+        try:
+            a = float(input('Введите первое: '))
+            b = float(input('Введите второе число: '))
+            if s == '+':
+                print(f'{a + b}')
+            elif s == '-':
+                print(f'{a - b}')
+            elif s == '*':
+                print(f'{a * b}')
+            elif s == '/':
+                if b != 0:
+                    print(f'{a / b}')
+                else:
+                    print('Деление на ноль!')
+        except(ValueError, TypeError):
+            print('вы ввели не верные данные')
+    else:
+        print('Неверный знак операции!')
